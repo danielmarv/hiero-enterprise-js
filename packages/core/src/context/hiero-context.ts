244e95a2185af09e5f31abc0183b32bba5ea46be
Jessie Ssebuliba <jessiessebuliba@gmail.com>
2026-05-24 13:56:38 +0300

    feat: Update type definitions for Node.js in sample applications and core context
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

--- packages/core/src/context/hiero-context.ts
+++ packages/core/src/context/hiero-context.ts
@@ -1,4 +1,5 @@
-import { Client, AccountId, PrivateKey, Transaction } from "@hiero-ledger/sdk";
+import type { Transaction } from "@hiero-ledger/sdk";
+import { Client, AccountId, PrivateKey } from "@hiero-ledger/sdk";
 import type { HieroConfig } from "../config/index.js";
 import { resolveConfigFromEnv, assertEnvConfigValid } from "../config/index.js";
 import { HieroError } from "../errors/index.js";
